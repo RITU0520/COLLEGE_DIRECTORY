940545d88274306df150a6f350f8193c1b4cc2cf
Ritu <151620263+RITU0520@users.noreply.github.com>
2024-09-25 03:29:41 +0530

    Delete College Directory/SB/myproject directory

--- College Directory/SB/myproject/src/main/java/com/example/myproject/CollegeDirectoryApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.myproject;
-
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@EnableJpaRepositories
-
-public class CollegeDirectoryApplication {
-    
-}
